91f87583948f2bc3c5d2c25216085d3778083758
Ramon Vianna <ramonw@poli.ufrj.br>
2015-03-02 18:55:34 -0300

    adicionando testes em views

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.db import models
 from django.utils import timezone
 
@@ -13,6 +15,9 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
+    def __unicode__(self):
+        return '%s' % self.question_text
 
 
 class Choice(models.Model):
